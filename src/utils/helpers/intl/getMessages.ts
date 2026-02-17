be573689fcfa5d02cdb0c2c2f68be1d7329c5eb9
MuratFaizulla <muratf345@gmail.com>
2026-02-17 12:13:52 +0500

    fix: move locales to public folder for production

--- src/utils/helpers/intl/getMessages.ts
+++ src/utils/helpers/intl/getMessages.ts
@@ -1,12 +1,27 @@
 import type { AcceptLocales } from './getLocale';
 import { DEFAULT_LOCALE } from './getLocale';
 
-export const getMessages = async (locale: AcceptLocales) => {
-  try {
-    const messages = await import(`/src/static/locales/${locale}.json`);
-    return messages.default; // ← не забудь .default
-  } catch (e) {
-    const defaultMessages = await import(`/src/static/locales/${DEFAULT_LOCALE}.json`);
-    return defaultMessages.default;
+type Messages = Record<string, string>;
+
+const localeModules = import.meta.glob<{
+  default: Messages;
+}>('../../../static/locales/*.json');
+
+export const getMessages = async (locale: AcceptLocales): Promise<Messages> => {
+  const modules = Object.keys(localeModules);
+
+  const localePath = modules.find((path) => path.endsWith(`${locale}.json`));
+
+  const fallbackPath = modules.find((path) => path.endsWith(`${DEFAULT_LOCALE}.json`));
+
+  const loader =
+    (localePath && localeModules[localePath]) || (fallbackPath && localeModules[fallbackPath]);
+
+  if (!loader) {
+    throw new Error('Default locale not found');
   }
+
+  const messages = await loader();
+
+  return messages.default; // ✅ больше не горит
 };
